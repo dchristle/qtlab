9b43d777e4b2e53ea5fdd2884e0ea50d1bde630e
dchristle <christle@uchicago.edu>
2013-11-11 19:34:49 -0600

    Create instruments updated

--- init/80_create_instruments.py
+++ init/80_create_instruments.py
@@ -1,8 +1,10 @@
-example1 = qt.instruments.create('example1', 'example', address='GPIB::1', reset=True)
+##example1 = qt.instruments.create('example1', 'example', address='GPIB::1', reset=True)
 dsgen = qt.instruments.create('dsgen', 'dummy_signal_generator')
 pos = qt.instruments.create('pos', 'dummy_positioner')
 combined = qt.instruments.create('combined', 'virtual_composite')
 combined.add_variable_scaled('magnet', example1, 'chA_output', 0.02, -0.13, units='mT')
+ddg = qt.instruments.create('ddg','SR_DG645',address='GPIB0::15::INSTR')
+tl = qt.instruments.create('tl','ThorLabs_ITC4001',address='USB0::0x1313::0x804A::M00277475::INSTR')
 #combined.add_variable_combined('waveoffset', [{
 #    'instrument': dmm1,
 #    'parameter': 'ch2_output',
